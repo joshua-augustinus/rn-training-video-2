d406b0b66306fac2923d2aa2a471a9c0f575cd09
joshua-augustinus <joshua.augustinus@gmail.com>
2020-11-01 16:02:52 +1100

    Working solution on Android

--- android/app/src/main/java/com/awesomeapp/MainApplication.java
+++ android/app/src/main/java/com/awesomeapp/MainApplication.java
@@ -14,6 +14,7 @@ import com.facebook.soloader.SoLoader;
 
 
 import java.util.List;
+import org.wonday.orientation.OrientationActivityLifecycle;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -48,5 +49,6 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    registerActivityLifecycleCallbacks(OrientationActivityLifecycle.getInstance());
   }
 }
